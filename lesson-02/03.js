660d158530622fd9f0f89c0e34ff0f2c84a98722
Shinkaruk Anna <sklifa1087@gmail.com>
2026-03-30 19:20:14 +0300

    commit task-1

--- lesson-02/03.js
+++ lesson-02/03.js
@@ -4,22 +4,20 @@
  * Результат сохраните в переменной grade.
  */
 
-const score = 53 // тестовое значение, можно изменять
+const score = 43 // тестовое значение, можно изменять
 let grade 
 // your code
-function  grade (score){
-if (score >=90 && score <=100) {
-return grade ="A";
-} else if (score >=80 && score <=89){
-    return grade="B";
-} else if (score >=70 && score <=79){
-    return grade="C";
-} else if (score >=50 && score <=69){
-    return grade="D";
-} else if (score >=0 && score <=49){
-    return grade="A";
-} else {
-    return ("нужно еще подучить! ")
-}
 
-}console.log(grade)
+if (score >=90 && score <=100) {
+    grade ="A";
+} else if (score >=80 && score <=89){
+    grade="B";
+} else if (score >=70 && score <=79){
+    grade="C";
+} else if (score >=50 && score <=69){
+    grade="D";
+} else if (score >=0 && score <=49){
+    grade="F";
+} 
+
+console.log(grade)
