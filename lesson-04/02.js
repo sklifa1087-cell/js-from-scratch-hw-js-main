d3ca393bfa2d651ca0db66ccd5dd2220c8fe2ab1
Shinkaruk Anna <sklifa1087@gmail.com>
2026-05-12 19:12:28 +0300

    commit task-2

--- lesson-04/02.js
+++ lesson-04/02.js
@@ -17,14 +17,18 @@
 let someArray = [1,2,3,4,5,3,2,4,9,9]
 
 function findUniqueElements(array) {
-    let newArray = [];
+    let newArray = []; //если ел из array не присутствует в newArray то мы должны этот эл добав в newArray push
     for (let i = 0; i< array.length; i++) {
-        for (let j = 0; j< i; j++)
-        if (array[i] === array[j]){
+        // for (let j = 0; j< i; j++)
+        // if (array[i] === array[j]){
+        //     newArray.push(array[i])
+        // }
+        if (!includesElement(newArray,array[i])) {
             newArray.push(array[i])
         }
     }
     return newArray;
 }
+
 newArray = findUniqueElements(someArray)
 console.log(newArray);
