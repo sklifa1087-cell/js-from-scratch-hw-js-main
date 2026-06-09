fde1febe1156464f84d1e8e799aa3d5ee3fa7e25
Shinkaruk Anna <sklifa1087@gmail.com>
2026-06-09 21:08:56 +0300

    commit task-1

--- lesson-05/02.js
+++ lesson-05/02.js
@@ -35,4 +35,12 @@ const gallery = {
   'The Scream': 'Edvard Munch',
 }
 
-function updateGallery() {}
+function updateGallery(gallery, title, newMeaning ) {
+    gallery[title] = newMeaning
+
+
+}
+updateGallery(gallery, 'Mona Lisa', 'Leonardo da Vinci, 1503-1506')
+updateGallery(gallery, 'The Persistence of Memory', 'Salvador Dali')
+
+console.log(gallery)
