a56205a08934f9c263a2f76e2cdf5155db647c24
Shinkaruk Anna <sklifa1087@gmail.com>
2026-07-12 19:25:41 +0300

    commit task-1

--- lesson-07/01.js
+++ lesson-07/01.js
@@ -16,4 +16,35 @@ const WEB_TECH_IMAGES = [
   'https://production-it-incubator.s3.eu-central-1.amazonaws.com/file-manager/Image/32f74d50-68d0-46aa-b035-7b3a5300d2c1_js-magic-logo.jpg',
   'https://production-it-incubator.s3.eu-central-1.amazonaws.com/file-manager/Image/c8a1f4a6-1337-4899-bdfd-a8c9c7bb806a_css-magic-logo.jpg',
   'https://production-it-incubator.s3.eu-central-1.amazonaws.com/file-manager/Image/784380b9-6937-42a6-bdfe-869835820234_html-magic-logo.jpg',
-]
+];
+
+const prevBtn = document.getElementById("prev-button");
+const nextBtn = document.getElementById("next-button");
+const imgElement = document.getElementById("web-tech-image");
+
+
+let currentIndex = 0;
+
+
+function updateImage() {
+  imageEl.src = WEB_TECH_IMAGES[currentIndex];
+}
+
+
+nextBtn.addEventListener('click', () => {
+  currentIndex = currentIndex + 1;
+  if (currentIndex >= WEB_TECH_IMAGES.length) {
+    currentIndex = 0;
+  }
+  updateImage();
+});
+
+prevBtn.addEventListener('click', () => {
+  currentIndex = currentIndex - 1;
+  if (currentIndex < 0) {
+    currentIndex = WEB_TECH_IMAGES.length - 1;
+  }
+  updateImage();
+});
+
+
